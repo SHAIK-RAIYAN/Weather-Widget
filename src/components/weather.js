117f2ebed362757e553a767fb8ac65b761399e56
SHAIK RAIYAN <shaikraiyan2005@gmail.com>
2025-08-04 15:44:20 +0530

    Deployment errors fixed ALL WORKING

--- src/components/weather.js
+++ src/components/weather.js
@@ -5,14 +5,14 @@ let getWeatherInfo = async (locationQuery) => {
     let api_key = import.meta.env.VITE_WEATHER_API_KEY;
     let response = await fetch(`${URL}?key=${api_key}&q=${locationQuery}`);
     const weatherData = await response.json();
-      if (!response.ok || data.error) {
-      throw new Error(data.error?.message || "Unknown API error.");
+    if (!response.ok || weatherData.error) {
+      throw new Error(weatherData.error?.message || "Unknown API error.");
     }
     return weatherData;
   } catch (error) {
     return {
       error: {
-        message: err.message || "Network error. Please check your connection",
+        message: error.message || "Network error. Please check your connection",
       },
     };
   }
